81bf5628f113fecfe687d5628fdacb386fbff35b
HrishabhRawat <hrishabhrawat100@gmail.con>
2026-05-08 01:41:20 +0530

    main structure and css

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,6 +8,30 @@ function Main() {
 				<p>Gemini</p>
 				<img src={assets.user_icon} alt=''/>
 			</div>
+            <div className='main-container'>
+                <div className='greet'>
+                    <p><span>Hello , Dev</span></p>
+                    <p>How can I Help You Today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on a upcoming road trip</p>
+                        <img src={assets.compass_icon}/>
+                    </div>
+                    <div className="card">
+                        <p>Breifly summarize this concept : urban planning</p>
+                        <img src={assets.bulb_icon}/>
+                    </div>
+                    <div className="card">
+                        <p>Brainstrom team bonding activites for our work retreat</p>
+                        <img src={assets.message_icon}/>
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon}/>
+                    </div>
+                </div>
+            </div>
 		</div>
 	)
 }
